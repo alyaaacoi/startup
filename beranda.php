b128e46f7b171f524f6bd035b31b50e03c1d4871
N3llySaf <nelivivo24@gmail.com>
2025-06-02 19:55:25 +0800

    alyaaa laparrrr

--- beranda.php
+++ beranda.php
@@ -27,7 +27,7 @@
                 <p>Ini adalah halaman beranda website startup.</p>
             </div>
             <article>
-                <h2>Apa itu Startup</h2>
+                <h2>Apa itu Nelly</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, nostrum veniam. Blanditiis beatae sunt minima tempora laborum vel dignissimos debitis consequuntur similique accusantium facere ad, possimus voluptatum natus ullam impedit?</p>
             </article>
         </div>
