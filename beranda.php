cbca97b954656b6388821b298118dd53e4c93f90
ARSAL21 <arsalkdm21@gmail.com>
2025-06-02 18:21:11 +0800

    dia yang pertama

---
+++ beranda.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beranda</title>
+</head>
+<body>
+    <div class="container">
+    <!-- header -->
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="beranda.php">Beranda</a></li>
+                    <li><a href="daftar-mahasiswa.php">Daftar Mahasiswa</a></li>
+                    <li><a href="tentang.php">Tentang</a></li>
+                    <li><a href="kontak.php">Kontak</a></li>
+                    <li><a href="logout.php">Log Out</a></li>
+                </ul>
+            </nav>
+        </header>
+        <!-- konten -->
+        <div class="konten">
+            <div class="hero">
+                <h1>Selamat Datang di Website Startup</h1>
+                <p>Ini adalah halaman beranda website startup.</p>
+            </div>
+            <article>
+                <h2>Apa itu Startup</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, nostrum veniam. Blanditiis beatae sunt minima tempora laborum vel dignissimos debitis consequuntur similique accusantium facere ad, possimus voluptatum natus ullam impedit?</p>
+            </article>
+        </div>
+        <!-- footer -->
+        <footer>
+            <p>&copy; 2025 Startup. All rights reserved.</p>
+        </footer>
+    </div>
+</body>
+</html>
